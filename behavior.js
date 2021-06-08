edc84ac6b0af43cdd97352a2f727a5ad1a1364d8
Gerlado Oneil Rosales Poma <rosalespoma15@gmail.com>
2021-06-08 17:15:28 -0500

    se inserto una imagen

--- behavior.js
+++ behavior.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded",function(event){
   console.log("Hello");
+  var buttons= document.getElementsByClassName("btn");
+  
+  function myFunction() {
+    var card1 = document.getElementById("card-1");
+    var card2 = document.getElementById("caed-2");
+    if (card1.style.display === "none") {
+      card1.style.display = "block";
+    } else {
+      card1.style.display = "none";
+    }
+
+  }
+  
+
   var thumbnailElement = document.getElementById("smart_thumbnail");
   thumbnailElement.addEventListener("click",function(){
     if(thumbnailElement.className==""){
