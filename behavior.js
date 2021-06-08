5357ab4c54a26ce66a9fb4de15526c65f160152a
rosales177 <rosalespoma15@gmail.com>
2021-06-08 13:10:18 -0500

    Se completo el archivo.js y se hizo cambios en la pagina adicional 1

--- behavior.js
+++ behavior.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded",function(event){
   console.log("Hello");
+  var thumbnailElement = document.getElementById("smart_thumbnail");
+  thumbnailElement.addEventListener("click",function(){
+    if(thumbnailElement.className==""){
+      thumbnailElement.className="small";
+    }
+    else{
+      thumbnailElement.className="";
+    }
+    alert("I saw you click!");
+  });
 })
